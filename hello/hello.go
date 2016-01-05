c9d8a54c94e8e67fb1c069fe31d56f94d0f587e5
brijwhiz <brij.programming@gmail.com>
2016-01-06 01:40:25 +0530

    More elaborate explanations of the Hello.go program

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,27 @@
+// single line comment. Line comments start with the
+// character sequence // and stop at the end of the line
+// A line comment acts like a newline.
+// Comments do not nest.
+
+/*
+   Multi-
+      line comment
+      */
+
+/*
+Package main.
+All Go files start with package <something>.
+package main is required for a standalone executable.
+*/
+
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+   //Mulitple statements separated by ;
+   fmt.Printf("Hello "); fmt.Printf("World\n")
+
+   // Single statement then ; not needed
+	fmt.Printf("Learning Go.\n")
 }
