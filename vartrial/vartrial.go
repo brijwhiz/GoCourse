14d84a1975d20045b360543de3ea1f6b8d68d592
brijwhiz <brij.programming@gmail.com>
2016-01-10 19:53:19 +0530

    declaring variables, scope and short declarations

---
+++ vartrial/vartrial.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var x int
+
+func main() {
+	p := 5
+	// var p int = 5 is longer form of the short declaration above
+
+	var q int
+	fmt.Println("p is:", p)
+	fmt.Println("q is: ", q)
+	fmt.Println("x is:", x)
+}
