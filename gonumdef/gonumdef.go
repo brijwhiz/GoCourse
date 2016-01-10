9899df4bd311e32836911e5b406da04b598bc7e2
brijwhiz <brij.programming@gmail.com>
2016-01-10 16:19:21 +0530

    Trying out somedefault value outputs

---
+++ gonumdef/gonumdef.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a1 int
+	var b1 bool
+	var f1 float64
+	var s1 string
+
+	fmt.Println("Type:    Initialized Value")
+	fmt.Println("Boolean: ", b1)
+	fmt.Println("Integer: ", a1)
+	fmt.Println("F Point: ", f1)
+	fmt.Println("String: ", s1)
+}
